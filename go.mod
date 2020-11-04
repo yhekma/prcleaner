f049ab3d2b8f3fed143ee418396f40947bfed6cc
Yoram Hekma <yoram.hekma@tnt-digital.com>
2020-11-04 13:32:32 +0100

    cleanup

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.az.fxei.fedex.com/3741836/prcleaner
 go 1.15
 
 require (
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/sirupsen/logrus v1.7.0
-	github.com/spf13/pflag v1.0.5
 	k8s.io/apimachinery v0.19.3
 	k8s.io/client-go v0.19.3
 )
--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"github.com/kelseyhightower/envconfig"
 	log "github.com/sirupsen/logrus"
-	flag "github.com/spf13/pflag"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"net/http"
 	"os"
 )
 
+type Config struct {
+	ReleaseLabel   string `default:"helm.sh/release"`
+	BranchLabel    string `default:"app.fedex.io/git-branch"`
+	CommitShaLabel string `default:"app.fedex.io/git-commit"`
+	RepoLabel      string `default:"app.fedex.io/git-repository"`
+	Dryrun         bool   `default:"true"`
+	Debug          bool   `default:"true"`
+}
+
 var (
-	ReleaseLabel   string
-	BranchLabel    string
-	CommitShaLabel string
-	RepoLabel      string
-	Dryrun         bool
-	Clientset      *kubernetes.Clientset
+	C         Config
+	Clientset *kubernetes.Clientset
 )
 
 func CheckErr(err error) {
@@ -40,67 +45,10 @@ func logInit() {
 
 func main() {
 	logInit()
-	debug := flag.BoolP("verbose", "v", false, "turn on verbose. Env: DEBUG")
-	dryrun := flag.BoolP("dry-run", "d", true, "don't actually do anything. Env: DRYRUN")
-	releaseLabel := flag.String("releaseLabel", "helm.sh/release", "label name for releases. Env: RELEASE_LABEL")
-	branchLabel := flag.String("branchLabel", "app.fedex.io/git-branch", "label name for branches. Env: BRANCH_LABEL")
-	repoLabel := flag.String("repoLabel", "app.fedex.io/git-repository", "label name for repo. Env: REPO_LABEL")
-	commitShaLabel := flag.String("commitSha", "app.fedex.io/git-commit", "label name for commit sha. Env: COMMIT_LABEL")
-	flag.Parse()
-
-	// TODO this needs to be moved to cobra/viper
-	if _, set := os.LookupEnv("DEBUG"); set || *debug {
-		log.Info("running in debug")
-		log.SetLevel(log.DebugLevel)
-	}
-
-	if *debug {
-		log.Info("running in verbose")
-		log.SetLevel(log.DebugLevel)
-	}
-
-	if _, set := os.LookupEnv("DRYRUN"); set {
-		log.Debug("set dryrun to true from env")
-		Dryrun = true
-	} else {
-		log.Debug("set dryrun to ", *dryrun, " from flag/default", *dryrun)
-		Dryrun = *dryrun
-	}
-
-	if r, set := os.LookupEnv("RELEASE_LABEL"); set {
-		log.Debug("set releaselabel to", r, " from env")
-		ReleaseLabel = r
-	} else {
-		log.Debug("set releaselabel to ", *releaseLabel, " from flag/default")
-		ReleaseLabel = *releaseLabel
-	}
-
-	if b, set := os.LookupEnv("BRANCH_LABEL"); set {
-		log.Debug("set branchlabel to ", b, " from env")
-		BranchLabel = b
-	} else {
-		log.Debug("set branchlabel to ", *branchLabel, " from flag/default")
-		BranchLabel = *branchLabel
-	}
-
-	if r, set := os.LookupEnv("REPO_LABEL"); set {
-		log.Debug("set repolabel to ", r, " from env")
-		RepoLabel = r
-	} else {
-		log.Debug("set repolabel to ", *repoLabel, " from flag/default")
-		RepoLabel = *repoLabel
-	}
-
-	if c, set := os.LookupEnv("COMMIT_LABEL"); set {
-		log.Debug("set commitlabel to ", c, " from env")
-		CommitShaLabel = c
-	} else {
-		log.Debug("set commitlabel to ", *commitShaLabel, " from flag/default")
-		CommitShaLabel = *commitShaLabel
-	}
+	err := envconfig.Process("cleaner", &C)
 
 	Clientset = getKubeCtx()
 	handler := http.HandlerFunc(CleanerServer)
-	err := http.ListenAndServe(":8000", handler)
+	err = http.ListenAndServe(":8000", handler)
 	CheckErr(err)
 }
